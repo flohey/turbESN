585832eaa0bd7f597ca054d5d04fce828a3a7b40
Florian Heyder <60064644+flohey@users.noreply.github.com>
2022-04-04 14:38:28 +0200

    Update cross_validation.py

--- turbESN/cross_validation.py
+++ turbESN/cross_validation.py
@@ -14,13 +14,14 @@ import h5py
 
 class CrossValidation():
 
-    def __init__(self,data: torch.Tensor, 
+    def __init__(self, 
                     esn: ESN,
                     n_folds: int,
                     fold_length: int=None, 
                     n_training_folds: int=2,
                     n_testing_folds: int=1, 
-                    n_validation_folds: int=1):
+                    n_validation_folds: int=1,
+                    data: torch.Tensor = None):
 
         logging.basicConfig(format=_LOGGING_FORMAT, level= esn.logging_level)
 
@@ -31,14 +32,15 @@ class CrossValidation():
         self.n_validation_folds = n_validation_folds
 
 
-        self.data_folded_u, self.data_folded_y = self.prepare_k_fold_walk_forward_validation_auto_data(data,
+        if data is not None:
+            self.data_folded_u, self.data_folded_y = self.prepare_k_fold_walk_forward_validation_auto_data(data,
                                                                                                   esn_start=esn.esn_start,
                                                                                                   esn_end=esn.esn_end,
                                                                                                   n_folds=self.n_folds,
                                                                                                   fold_length=self.fold_length)
 
-        if self.fold_length is None:
-            self.fold_length = data_folded_u.shape[1]
+            if self.fold_length is None:
+                self.fold_length = data_folded_u.shape[1]
 
     #--------------------------------------------------------------------------
     #FH 30.03.2022: added prepare_k_fold_walk_forward_validation_auto_data
@@ -86,19 +88,75 @@ class CrossValidation():
         '''
 
         if f is None:
-            f = h5py.File(filepath, 'w')
+            f = h5py.File(filepath, 'a')
             toClose = True
         
         logging.warn('Saving CrossValidation parameters to Hdf5 file {0}'.format(filepath))
+
+        G_hp = f.get('Hyperparameters')
+        G_hp.attrs['n_training_folds'] = self.n_training_folds
+        G_hp.attrs['n_testing_folds'] = self.n_testing_folds
+        G_hp.attrs['n_validation_folds'] = self.n_validation_folds
 
         G_data = f.get("Data")
         if G_data is None:
             G_data = f.create_group('Data')
 
-        #Datasets
         G_data.create_dataset('data_folded_u',   data = self.data_folded_u, compression = 'gzip', compression_opts = 9)
         G_data.create_dataset('data_folded_y',   data = self.data_folded_y, compression = 'gzip', compression_opts = 9)
 
         
         if toClose:
             f.close()
+
+
+
+
+#--------------------------------------------------------------------------     
+#--------------------------------------------------------------------------
+#  CLASS METHODS
+#--------------------------------------------------------------------------
+#--------------------------------------------------------------------------
+    @classmethod
+    def read(cls, filepath: str, esn: ESN):
+
+
+        try:
+            with h5py.File(filepath,'r') as f:
+                pass
+        except:
+            logging.debug('Error: file {0} not found.'.format(filepath))
+        
+        
+        with h5py.File(filepath,'r') as f:
+
+            G_hp = f.get('Hyperparameters')
+            n_training_folds = G_hp.attrs['n_training_folds']
+            n_testing_folds = G_hp.attrs['n_testing_folds']
+            n_validation_folds = G_hp.attrs['n_validation_folds']
+
+            G_data = f.get('Data')
+            if 'data_folded_u' in G_data:
+                data_folded_u = np.array(G_data.get('data_folded_u'))
+                n_folds, fold_length, _ = data_folded_u.shape
+            else:
+                data_folded_u = None
+
+            if 'data_folded_y' in G_data:
+                data_folded_y = np.array(G_data.get('data_folded_y'))                                    
+            else:
+                data_folded_y = None
+
+            assert data_folded_u is not None, "Error: couldnt find data_folded_u"
+            assert data_folded_y is not None, "Error: couldnt find data_folded_y"
+            
+            cv = CrossValidation(esn =esn,
+                                n_folds=n_folds,
+                                fold_length=fold_length, 
+                                n_training_folds=n_training_folds,
+                                n_testing_folds=n_testing_folds, 
+                                n_validation_folds=n_validation_folds)
+
+            cv.data_folded_u = data_folded_u
+            cv.data_folded_y = data_folded_y
+        return cv
